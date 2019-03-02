43b48dc11971184276180dc00733c62754e55d65
Martin Gorski <gorskigoral@gmail.com>
2019-03-02 19:43:37 +0100

    init

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  plumber = require("gulp-plumber"),
+  browserSync = require("browser-sync");
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./scss/**/style.scss")
+    .pipe(plumber())
+    .pipe(sass.sync())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 3 version", "IE 11"]
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./scss/**/*.scss", ["sass"]);
+  gulp.watch(["./index.html", "./js/*.js"]).on("change", browserSync.reload);
+});
+
+gulp.task("browser-sync", function() {
+  // browserSync.init({
+  //   proxy: "localhost/rpgoals"
+  // });
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task("default", ["sass", "browser-sync", "watch"]);
